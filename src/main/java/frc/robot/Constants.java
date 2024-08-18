3fbbb9f1bc6f6d7e410c4ee608feb8834d81ff7d
320948-AAPS <147006693+320948-AAPS@users.noreply.github.com>
2024-08-17 22:05:49 -0400

    Delete constants for shooter motor speeds

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -42,7 +42,4 @@ public final class Constants {
   public static class ShooterConstants {
     public static final int kLeftShooterMotorID = 0;
     public static final int kRightShooterMotorID = 0;
-
-    public static final double kLeftShooterMotorSpeed = 0;
-    public static final double kRightShooterMotorSpeed = 0;
 }
